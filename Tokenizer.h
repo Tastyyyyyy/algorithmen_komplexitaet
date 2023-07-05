7a048a27a553ef7e9daced90b239a027cb97be54
lucaladerer <luca.laderer2002@gmail.com>
2023-07-05 13:17:23 +0200

    tokenizer versucht

--- Tokenizer.h
+++ Tokenizer.h
@@ -8,32 +8,85 @@
 
 using namespace std;
 
-class Tokenizer 
+class Tokenizer 		//bearbeitet von Luca Laderer
 {
  
     string src;
 
 public:
-    Tokenizer(string s) 
+    Tokenizer(string s) 	//Konstruktor wird mit string als Argument aufgerufen
 	{
         src = s + " "; // Das Leerzeichen dient dazu, beim Lesen von Zahlen nicht 
 					   // ueber die String-Grenze zu laufen
+		goThroughString();
     }
+
+
+
+	void goThroughString()
+	{
+		int i = 0;
+		while(src[i] != ' ')
+		{
+			char test = src[i];
+			tokenize(test);
+			i++;
+		}
+	}
+
+	bool checkNum()
+	{
+		if(src[i] == "0" || src[i] == "1" || src[i] == "2" || src[i] == "3" || src[i] == "4" || src[i] == "5" || src[i] == "6" || src[i] == "7" || src[i] == "8" || src[i] == "9")
+		{
+
+		}
+		else
+		{
+			checkNum();
+		}
+		else
+		{
+			checkBracket();
+		}
+	}
+	
+	bool checkOp()
+	{
+		if(src[i] == "+" || src[i] == "-" || src[i] == "*" || src[i] == "/")
+		{
+
+		}
+	}
+
+	bool checkBracket()
+	{
+		if(src[i] == "(" || src[i] == ")")
+		{
+
+		}
+	}
     
-    vector<Token*>* tokenize() 
+
+    vector<Token*>* tokenize(char s) 
 	{
         vector<Token*>* tokens = new vector<Token*>();
 	    vector<Token*>::iterator i = tokens->begin();
-
+		tokens->push_back(s);
 		/*
 
-		Beispiel für das Einfügen von Knoten:
+		Beispiel fï¿½r das Einfï¿½gen von Knoten:
 
 		i = tokens->insert(i, new Op(c));
 		i = tokens->insert(i, new Num(c));
 		usw. 
 		
 		*/
+		switch()
+				{
+					case :
+					break;
+				}
+
 		
 		// to implement ...
 
